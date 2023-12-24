802b57ea4517be3e0777e2aa8660da74462b53cb
jonathan <aungzayar1352000@gmail.com>
2023-12-24 22:49:50 +0630

    specified to do

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,48 @@
 import { useEffect, useState } from "react";
 
 const App = () => {
-  const [todos, setTodos] = useState([]);
-  useEffect((_) => {
-    fetchData();
-  },[]);
+  const [id, setId] = useState("");
+  const [error, setError] = useState(false);
+  const [todo, setTodo] = useState(undefined);
+  const getData = async (e) => {
+    e.preventDefault();
 
-  const fetchData = async () => {
-    const response = await fetch("https://jsonplaceholder.typicode.com/todos")
+    if(id < 1){
+      setError(true);
+      setId("");
+      return;
+    }
+
+    const response = await fetch(`https://jsonplaceholder.typicode.com/todos/${id}`)
     const data = await response.json();
-    setTodos(data);
+    setError(false);
+    setTodo(data);
+    setId("");
   }
 
   return(
   <>
   <section>
-    <table>
-      <thead>
-        <tr>
-          <th>id</th>
-          <th>title</th>
-          <th>completed</th>
-        </tr>
-      </thead>
-
-      <tbody>
-        {todos.map(todo=> (
-          <tr key={todo.id}>
-            <td>{todo.id}</td>
-            <td>{todo.title}</td>
-            {
-              todo.completed ? (<p>Done</p>) : (<p className="none">None</p>)
-            }
-          </tr>
-        ))}
-      </tbody>
-    </table>
+    <form onSubmit={getData}>
+      <input type="number" value={id} onChange={(e)=>setId(e.target.value)}/>
+      <button type="submit">Get Data</button>
+    </form>
+    <div>
+      {
+        error && (
+          <h1>Please enter a valid id. (example 1, 2, 3,...)</h1>
+        )
+      }
+      {todo && (
+      <div>
+        <h1>id - {todo.id}</h1>
+        <h1>titile - {todo.title}</h1>
+        <h1>userId - {todo.userId}</h1>
+        <h1>completed - {todo.completed ? (<span>Completed</span>) : (<span>Not completed</span>)}</h1>
+      </div>
+      )
+      }
+    </div>
   </section>
   </>
   )
