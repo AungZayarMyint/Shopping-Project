edcd8e2cbb75438ac74afc3708aa4e3c3bebf881
jonathan <aungzayar1352000@gmail.com>
2023-12-24 10:45:41 +0630

    useState Hook

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+
+const App = () => {
+  const [key, setKey] = useState("");
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <input type="text" onChange={(e) => {
+        setKey(e.target.value);
+      }}/>
+      <h1>Your key is - {key}</h1>
+      <button onClick={() => {
+        setCount((perv)=>perv - 1);
+        }}
+      >
+        -
+      </button>
+      <span>{count}</span>
+      <button onClick={()=>{
+        setCount((prev)=> prev + 1);
+      }}>+</button>
+    </div>
+  );
+}
+
+export default App;
